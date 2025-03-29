a36f09a2ef90cb68cad9d609497c4ec6c8f34583
Matteo Braceschi <matteobraceschi@gmail.com>
2025-03-29 18:45:48 +0100

    change japan img

--- src/app/sections/PhotoTimeline.tsx
+++ src/app/sections/PhotoTimeline.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const photos = [
   { src: "/images/gruppo.jpg", text: "Friends" },
-  { src: "/images/japan_ghibli_gpt.png", text: "Japan Trip" },
+  { src: "/images/japan.png", text: "Japan Trip" },
 ];
 
 export default function PhotoTimeline() {
